737b1451a98664cc8e5a611c771654f49b61af72
CheezItMan <chris@adadevelopersacademy.org>
2018-07-13 10:18:15 -0700

    first test, checking return-type

--- clock_spec.rb
+++ clock_spec.rb
@@ -6,10 +6,9 @@ require_relative 'clock'
 
 Minitest::Reporters.use!
 
-
 describe "Clock" do
-
   it "will return a string" do
-
+    # the `clock` method must return a string
+    expect(clock() ).must_be_instance_of String
   end
 end
