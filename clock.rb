56259f91df17a3f6e18872fd661db8eddfc93786
CheezItMan <chris@adadevelopersacademy.org>
2018-07-18 19:12:31 -0700

    returning string formatted in a digital clock manner

--- clock.rb
+++ clock.rb
@@ -1,5 +1,15 @@
 # clock.rb
 
 def clock(hours, minutes, seconds)
-  return ''
+  time_fields = [hours, minutes, seconds]
+
+  time_fields.map! do |field|
+    if field < 10
+      "0#{field}"
+    else
+      "#{field}"
+    end
+  end
+
+  return "#{time_fields[0]}:#{time_fields[1]}:#{time_fields[2]}"
 end
