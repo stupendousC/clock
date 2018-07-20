0bab3855cce1cf56a13d5b329ff30000602bc3fc
CheezItMan <chris@adadevelopersacademy.org>
2018-07-20 08:58:05 -0700

    added test-case for arguments which are too large.

--- clock.rb
+++ clock.rb
@@ -2,6 +2,13 @@
 
 def clock(hours, minutes, seconds)
   time_fields = [hours, minutes, seconds]
+  max_values = [23, 59, 59]
+
+  time_fields.each_with_index do |field, index|
+    if field > max_values[index]
+      raise ArgumentError.new("#{field} is too large")
+    end
+  end
 
   time_fields.map! do |field|
     if field < 10
@@ -13,3 +20,6 @@ def clock(hours, minutes, seconds)
 
   return "#{time_fields[0]}:#{time_fields[1]}:#{time_fields[2]}"
 end
+
+
+clock(23, 59, 59)
