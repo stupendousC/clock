a78030a0a5e096df0d411909fa3ca0869ef03ae5
Caroline <CarolineWuKaplan@gmail.com>
2019-08-14 09:13:02 -0700

    test passed 100%

--- specs/clock_spec.rb
+++ specs/clock_spec.rb
@@ -13,48 +13,51 @@ describe "DOES CLOCK() WORK?" do
   #ARRANGE#     prep for code to be tested
   #ACT#         sample run of the code 
   #ASSERT#    test against what we EXPECT  
-
+  
   # LITTLE IDEAS: use IT block, inside DESCRIBE
   it "I WANT A STRING" do
     result = clock(1, 23, 45)
     expect(result).must_be_instance_of String
   end
-
+  
   it "I WANT HH:MM:SS FORMAT" do
     result = clock(12, 23, 45)
     expect(result).must_equal "12:23:45"
   end
-
+  
   it "CATCHES HH < 1" do
-    result = clock(0,10,30)
-    expect result.must_raise ArgumentError
+    expect {
+      clock(0,10,30)
+    }.must_raise ArgumentError
   end
-
+  
   it "CATCHES HH > 12" do
-    result = clock(13, 1, 2)
-    expect result.must_raise ArgumentError 
+    expect {
+      clock(13, 1, 2)
+    }.must_raise ArgumentError 
   end
-
+  
   it "CATCHES MM < 0" do
-    result = clock(1, -5, 3)
-    expect result.must_raise ArgumentError 
+    expect {
+      clock(1, -5, 3)
+    }.must_raise ArgumentError 
   end
-
+  
   it "CATCHES MM > 59" do
-    result = clock(1, 200, 3)
-    expect result.must_raise ArgumentError 
+    expect {
+      clock(1, 200, 3)
+    }.must_raise ArgumentError 
   end
-
+  
   it "CATCHES SS < 0" do
-    result = clock(1, 5, -3)
-    expect result.must_raise ArgumentError 
+    expect {
+      clock(1, 5, -3)
+    }.must_raise ArgumentError 
   end
-
+  
   it "CATCHES SS > 59" do
-    result = clock(1, 20, 300)
-    expect result.must_raise ArgumentError 
+    expect {
+      clock(1, 20, 300)
+    }.must_raise ArgumentError 
   end
 end
-
-puts "\n\n!!! BURNING QUESTION !!!"
-puts "Does RED mean something bad?  Maybe I'm not reporting it the exact right way?  Shouldn't it have been GREEN b/c I got what I expected?\n\n"
